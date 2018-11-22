d773d086916cb94008de230b2c9d2aed333cabb7
Wojtek Mach <wojtek@wojtekmach.pl>
2018-11-22 17:59:38 +0100

    Warn on :pool_timeout option

--- lib/ecto/adapters/sql.ex
+++ lib/ecto/adapters/sql.ex
@@ -444,6 +444,15 @@ defmodule Ecto.Adapters.SQL do
   end
 
   defp adapter_config(config) do
+    if Keyword.has_key?(config, :pool_timeout) do
+      message = """
+      :pool_timeout option no longer has an effect and is replaced with improved queuing system.
+      See \"Queue config\" in DBConnection.start_link/2 documentation for more information.
+      """
+
+      IO.warn(message)
+    end
+
     config
     |> Keyword.delete(:name)
     |> Keyword.update(:pool, DBConnection.ConnectionPool, &normalize_pool/1)
