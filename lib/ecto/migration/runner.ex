7ee02975adb4c370a3c52dfb03b1f8e48918c2f1
José Valim <jose.valim@gmail.com>
2018-11-26 23:05:51 +0100

    Update runner.ex

--- lib/ecto/migration/runner.ex
+++ lib/ecto/migration/runner.ex
@@ -23,9 +23,7 @@ defmodule Ecto.Migration.Runner do
     metadata(runner, opts)
 
     log(level, "== Running #{version} #{inspect module}.#{operation}/0 #{direction}")
-    {time1, _} = :timer.tc(fn -> perform_operation(repo, module, operation) end)
-    {time2, _} = :timer.tc(&flush/0, [])
-    time = time1 + time2
+    {time, _} = :timer.tc(fn -> perform_operation(repo, module, operation) end)
     log(level, "== Migrated #{version} in #{inspect(div(time, 100_000) / 10)}s")
 
     stop()
@@ -265,7 +263,7 @@ defmodule Ecto.Migration.Runner do
   ## Helpers
 
   defp perform_operation(repo, module, operation) do
-    if repo.in_transaction?() do
+    if function_exported?(repo, :in_transaction?, 0) and repo.in_transaction?() do
       if function_exported?(module, :after_begin, 0) do
         module.after_begin()
       end
@@ -280,6 +278,8 @@ defmodule Ecto.Migration.Runner do
     else
       apply(module, operation, [])
     end
+
+    flush()
   end
 
   defp runner do
