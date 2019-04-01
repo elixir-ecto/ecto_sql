c8a0ae01dd1c78a4b37e80722a7f62ee0c972cca
Bartlomiej Rozanski <ebartlomiej@gmail.com>
2019-04-01 14:10:38 +0200

    tests description

--- test/ecto/runner_test.exs
+++ test/ecto/runner_test.exs
@@ -5,8 +5,8 @@ defmodule Ecto.Migration.RunnerTest do
 
   alias EctoSQL.TestRepo
 
-  describe "repo_name" do
-    test "get by repo name" do
+  describe "repo_name/0" do
+    test "runner repo_name" do
       {:ok, runner} = start_link(self(), TestRepo, :tenant_db, :forward, :up, %{level: false, sql: false})
       Process.put(:ecto_migration, %{runner: runner, prefix: nil})
 
@@ -14,7 +14,7 @@ defmodule Ecto.Migration.RunnerTest do
       stop()
     end
 
-    test "get when runner is not started" do
+    test "when runner is not started" do
       assert_raise RuntimeError, ~r/could not find migration runner process/, fn ->
         repo_name()
       end
