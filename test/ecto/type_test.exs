8d8fe370ca6be18a15c100d6ab5d4a1911802c58
Milan Jaric <milan.jaric@gmail.com>
2020-01-13 23:43:22 +0100

    UUID type tests for mssql adapter

--- test/ecto/type_test.exs
+++ test/ecto/type_test.exs
@@ -2,10 +2,11 @@ defmodule Ecto.TypeTest do
   use ExUnit.Case, async: true
 
   import Ecto.Type
-  alias Ecto.Adapters.{MyXQL, Postgres}
+  alias Ecto.Adapters.{MyXQL, Postgres, MsSql}
 
   @uuid_string "bfe0888c-5c59-4bb3-adfd-71f0b85d3db7"
   @uuid_binary <<191, 224, 136, 140, 92, 89, 75, 179, 173, 253, 113, 240, 184, 93, 61, 183>>
+  @mssql_uuid_binary <<140, 136, 224, 191, 89, 92, 179, 75, 173, 253, 113, 240, 184, 93, 61, 183>>
 
   # We don't effectively dump because we need to keep JSON encoding
   test "dumps through the adapter" do
@@ -14,6 +15,12 @@ defmodule Ecto.TypeTest do
 
     assert adapter_dump(Postgres, {:map, Ecto.UUID}, %{"a" => @uuid_string}) ==
            {:ok, %{"a" => @uuid_string}}
+
+    assert adapter_dump(MsSql, {:map, Ecto.UUID}, %{"a" => @uuid_string}) ==
+           {:ok, ~s({"a":"#{@uuid_string}"})}
+
+    assert adapter_dump(MsSql, {:map, Tds.Types.UUID}, %{"a" => @uuid_string}) ==
+           {:ok, ~s({"a":"#{@uuid_string}"})}
   end
 
   # Therefore we need to support both binaries and strings when loading
@@ -24,10 +31,16 @@ defmodule Ecto.TypeTest do
     assert adapter_load(Postgres, {:map, Ecto.UUID}, %{"a" => @uuid_binary}) ==
            {:ok, %{"a" => @uuid_string}}
 
+    assert adapter_load(MsSql, {:map, Tds.Types.UUID}, %{"a" => @mssql_uuid_binary}) ==
+           {:ok, %{"a" => @uuid_string}}
+
     assert adapter_load(MyXQL, {:map, Ecto.UUID}, %{"a" => @uuid_string}) ==
            {:ok, %{"a" => @uuid_string}}
 
     assert adapter_load(Postgres, {:map, Ecto.UUID}, %{"a" => @uuid_string}) ==
+           {:ok, %{"a" => @uuid_string}}
+
+    assert adapter_load(MsSql, {:map, Tds.Types.UUID}, %{"a" => @uuid_string}) ==
            {:ok, %{"a" => @uuid_string}}
   end
 end
