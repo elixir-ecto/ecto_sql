583f84b4b5cd19bf33b5a7884a89b6eb679b86f4
Jerrad Christian <jerradchristian@gmail.com>
2020-03-02 12:17:29 -0500

    Fix deprecation warning for load_file

--- lib/ecto/migrator.ex
+++ lib/ecto/migrator.ex
@@ -589,7 +589,7 @@ defmodule Ecto.Migrator do
   end
 
   defp load_migration!({version, _, file}) when is_binary(file) do
-    loaded_modules = file |> Code.load_file() |> Enum.map(&elem(&1, 0))
+    loaded_modules = file |> Code.require_file() |> Enum.map(&elem(&1, 0))
 
     if mod = Enum.find(loaded_modules, &migration?/1) do
       {version, mod}
