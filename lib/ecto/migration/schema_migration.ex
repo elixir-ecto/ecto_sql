f476207ee05c7900ccff79c6b214fba5228a67b0
Sebastian Abondano <sabondano1@gmail.com>
2020-07-13 11:23:29 -0400

    store migration_repo in var to not compute twice

--- lib/ecto/migration/schema_migration.ex
+++ lib/ecto/migration/schema_migration.ex
@@ -15,9 +15,10 @@ defmodule Ecto.Migration.SchemaMigration do
   @opts [timeout: :infinity, log: false]
 
   def ensure_schema_migrations_table!(repo, opts) do
+    migration_repo = get_migration_repo(repo)
     table_name = repo |> get_source |> String.to_atom()
     table = %Ecto.Migration.Table{name: table_name, prefix: opts[:prefix]}
-    meta = Ecto.Adapter.lookup_meta(get_migration_repo(repo).get_dynamic_repo())
+    meta = Ecto.Adapter.lookup_meta(migration_repo.get_dynamic_repo())
 
     commands = [
       {:add, :version, :bigint, primary_key: true},
@@ -25,7 +26,7 @@ defmodule Ecto.Migration.SchemaMigration do
     ]
 
     # DDL queries do not log, so we do not need to pass log: false here.
-    get_migration_repo(repo).__adapter__.execute_ddl(meta, {:create_if_not_exists, table, commands}, @opts)
+    migration_repo.__adapter__.execute_ddl(meta, {:create_if_not_exists, table, commands}, @opts)
   end
 
   def versions(repo, prefix) do
