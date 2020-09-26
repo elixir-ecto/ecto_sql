85db8672dd370d4361d8196f0571029bb5412e27
Leandro Cesquini Pereira <leandro@leandro.io>
2020-09-26 10:59:32 -0400

    Execute STATISTICS to obtain the execution plan

--- integration_test/tds/explain_test.exs
+++ integration_test/tds/explain_test.exs
@@ -3,10 +3,34 @@ defmodule Ecto.Integration.ExplainTest do
 
   alias Ecto.Integration.TestRepo
   alias Ecto.Integration.Post
+  import Ecto.Query, only: [from: 2]
 
-  test "explain options" do
-    assert_raise(Tds.Error, "EXPLAIN is not supported by Ecto.Adapters.TDS at the moment", fn ->
-      TestRepo.explain(:all, Post)
-    end)
+  describe "explain" do
+    test "select" do
+      explain = TestRepo.explain(:all, from(p in Post, where: p.title == "explain_test", limit: 1))
+      assert explain =~ "| Rows | Executes |"
+      assert explain =~ "| Parallel | EstimateExecutions |"
+      assert explain =~ "SELECT TOP(1)"
+      assert explain =~ "explain_test"
+    end
+
+    test "delete" do
+      explain = TestRepo.explain(:delete_all, Post)
+      assert explain =~ "DELETE"
+      assert explain =~ "p0"
+    end
+
+    test "update" do
+      explain = TestRepo.explain(:update_all, from(p in Post, update: [set: [title: "new title"]]))
+      assert explain =~ "UPDATE"
+      assert explain =~ "p0"
+      assert explain =~ "new title"
+    end
+
+    test "invalid" do
+      assert_raise(Tds.Error, fn ->
+        TestRepo.explain(:all, from(p in "posts", select: p.invalid, where: p.invalid == "title"))
+      end)
+    end
   end
 end
--- lib/ecto/adapters/tds/connection.ex
+++ lib/ecto/adapters/tds/connection.ex
@@ -299,11 +299,74 @@ if Code.ensure_loaded?(Tds) do
       ]
     end
 
-    # TODO: see https://github.com/elixir-ecto/ecto_sql/pull/231#discussion_r433858288
     @impl true
-    def explain_query(_conn, _query, _params, _opts) do
-      raise Tds.Error, "EXPLAIN is not supported by Ecto.Adapters.TDS at the moment"
+    def explain_query(conn, query, params, opts) do
+      params = prepare_params(params)
+
+      case Tds.query_multi(conn, build_explain_query(query), params, opts) do
+        {:ok, [_, %Tds.Result{} = result, _]} ->
+          {:ok, format_result_as_table(result.columns, result.rows)}
+
+        error ->
+          error
+      end
     end
+
+    def build_explain_query(query) do
+      [
+        "SET STATISTICS XML ON; ",
+        "SET STATISTICS PROFILE ON; ",
+        query,
+        "; ",
+        "SET STATISTICS XML OFF; ",
+        "SET STATISTICS PROFILE OFF;"
+      ]
+      |> IO.iodata_to_binary()
+    end
+
+    defp format_result_as_table(columns, rows) do
+      column_widths =
+        [columns | rows]
+        |> List.zip()
+        |> Enum.map(&Tuple.to_list/1)
+        |> Enum.map(fn column_with_rows ->
+          column_with_rows |> Enum.map(&binary_length/1) |> Enum.max()
+        end)
+
+      [
+        separator(column_widths),
+        "\n",
+        cells(columns, column_widths),
+        "\n",
+        separator(column_widths),
+        "\n",
+        Enum.map(rows, &cells(&1, column_widths) ++ ["\n"]),
+        separator(column_widths)
+      ]
+      |> IO.iodata_to_binary()
+    end
+
+    defp binary_length(nil), do: 4 # NULL
+    defp binary_length(binary) when is_binary(binary), do: String.length(binary)
+    defp binary_length(other), do: other |> inspect() |> String.length()
+
+    defp separator(widths) do
+      Enum.map(widths, & [?+, ?-, String.duplicate("-", &1), ?-]) ++ [?+]
+    end
+
+    defp cells(items, widths) do
+      cell =
+        [items, widths]
+        |> List.zip()
+        |> Enum.map(fn {item, width} -> [?|, " ", format_item(item, width) , " "] end)
+
+      [cell | [?|]]
+    end
+
+    defp format_item(nil, width), do: String.pad_trailing("NULL", width)
+    defp format_item(item, width) when is_binary(item), do: String.pad_trailing(item, width)
+    defp format_item(item, width) when is_number(item), do: item |> inspect() |> String.pad_leading(width)
+    defp format_item(item, width), do: item |> inspect() |> String.pad_trailing(width)
 
     ## Query generation
 
