391190f404647ad6989e7faf35b26f59ab5afe25
Simon Jesenko <simon.jesenko@gmail.com>
2021-03-04 19:51:08 +0100

    Avoid passing `:socket` option to psql
    It is not supported as `--host` param. Warn instead and try connecting using
    other params - it will error anyway if connection cannot be established.
    
    Co-authored-by: Wojtek Mach <wojtekmach@users.noreply.github.com>

--- lib/ecto/adapters/postgres.ex
+++ lib/ecto/adapters/postgres.ex
@@ -343,7 +343,15 @@ defmodule Ecto.Adapters.Postgres do
     args =
       if port = opts[:port], do: ["-p", to_string(port)|args], else: args
 
-    host = opts[:socket] || opts[:socket_dir] || opts[:hostname] || System.get_env("PGHOST") || "localhost"
+    host = opts[:socket_dir] || opts[:hostname] || System.get_env("PGHOST") || "localhost"
+
+    if opts[:socket] do
+      IO.warn("""
+      Using `:socket` connection option is not supported by some operations on `Ecto.Adapters.Postgres`.
+      Trying to connect using fallback options...
+      """)
+    end
+
     args = ["--host", host|args]
     args = args ++ opt_args
     System.cmd(cmd, args, env: env, stderr_to_stdout: true)
