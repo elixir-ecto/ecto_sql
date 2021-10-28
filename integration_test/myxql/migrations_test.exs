5f6c8e6706bf004370223c92eaa93b0935605bd6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2021-10-28 13:47:33 -0500

    Remove transaction logs from MyXQL integration test.

--- integration_test/myxql/migrations_test.exs
+++ integration_test/myxql/migrations_test.exs
@@ -25,62 +25,64 @@ defmodule Ecto.Integration.MigrationsTest do
     @version_insert ~s[INSERT INTO `schema_migrations`]
     @version_delete ~s[DELETE s0.* FROM `schema_migrations`]
 
-    test "logs locking and transaction commands" do
+    test "logs locking commands" do
       num = @base_migration + System.unique_integer([:positive])
+
       up_log =
         capture_log(fn ->
-          Ecto.Migrator.up(PoolRepo, num, NormalMigration, log_migrator_sql: :info, log_migrations_sql: :info, log: :info)
+          Ecto.Migrator.up(PoolRepo, num, NormalMigration,
+            log_migrator_sql: :info,
+            log_migrations_sql: :info,
+            log: :info
+          )
         end)
 
-      assert up_log =~ "begin []"
       assert up_log =~ @get_lock_command
       assert up_log =~ @create_table_sql
       assert up_log =~ @create_table_log
       assert up_log =~ @release_lock_command
       assert up_log =~ @version_insert
-      assert up_log =~ "commit []"
 
       down_log =
         capture_log(fn ->
-          Ecto.Migrator.down(PoolRepo, num, NormalMigration, log_migrator_sql: :info, log_migrations_sql: :info, log: :info)
+          Ecto.Migrator.down(PoolRepo, num, NormalMigration,
+            log_migrator_sql: :info,
+            log_migrations_sql: :info,
+            log: :info
+          )
         end)
 
-      assert down_log =~ "begin []"
       assert down_log =~ @get_lock_command
       assert down_log =~ @drop_table_sql
       assert down_log =~ @drop_table_log
       assert down_log =~ @release_lock_command
       assert down_log =~ @version_delete
-      assert down_log =~ "commit []"
     end
 
     test "does not log sql when log is default" do
       num = @base_migration + System.unique_integer([:positive])
+
       up_log =
         capture_log(fn ->
           Ecto.Migrator.up(PoolRepo, num, NormalMigration, log: :info)
         end)
 
-      refute up_log =~ "begin []"
       refute up_log =~ @get_lock_command
       refute up_log =~ @create_table_sql
       assert up_log =~ @create_table_log
       refute up_log =~ @release_lock_command
       refute up_log =~ @version_insert
-      refute up_log =~ "commit []"
 
       down_log =
         capture_log(fn ->
           Ecto.Migrator.down(PoolRepo, num, NormalMigration, log: :info)
         end)
 
-      refute down_log =~ "begin []"
       refute down_log =~ @get_lock_command
       refute down_log =~ @drop_table_sql
       assert down_log =~ @drop_table_log
       refute down_log =~ @release_lock_command
       refute down_log =~ @version_delete
-      refute down_log =~ "commit []"
     end
   end
 end
