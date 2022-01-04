755504e8c67b8ee0a9be07c64873d7d462835652
Greg Rychlewski <grychle@Gregs-MacBook-Pro.local>
2022-01-04 00:06:23 -0500

    improve integration test

--- integration_test/pg/prepare_test.exs
+++ integration_test/pg/prepare_test.exs
@@ -8,6 +8,11 @@ defmodule Ecto.Integration.PrepareTest do
     one = TestRepo.insert!(%Post{title: "one"})
     two = TestRepo.insert!(%Post{title: "two"})
 
+    # Uncached
+    assert TestRepo.all(Post, prepare: :unnamed) == [one, two]
+    assert TestRepo.all(Post, prepare: :named) == [one, two]
+
+    # Cached
     assert TestRepo.all(Post, prepare: :unnamed) == [one, two]
     assert TestRepo.all(Post, prepare: :named) == [one, two]
   end
