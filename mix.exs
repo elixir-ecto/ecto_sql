c73ba2b1edf9d049804c2103d7dd4c9d28e6c59d
José Valim <jose.valim@dashbit.co>
2022-04-27 17:39:41 +0200

    Update Ecto dependency

--- mix.exs
+++ mix.exs
@@ -77,7 +77,7 @@ defmodule EctoSQL.MixProject do
     if path = System.get_env("ECTO_PATH") do
       {:ecto, path: path}
     else
-      {:ecto, "~> 3.8.0"}
+      {:ecto, "~> 3.8.1"}
     end
   end
 
