c307a1aea41ad4cd529247e4cf3310def5e03c14
Antonio <aschiavon91@gmail.com>
2022-11-23 09:29:36 -0300

    chore: ensure target versions are integer

--- lib/ecto/migrator.ex
+++ lib/ecto/migrator.ex
@@ -537,7 +537,7 @@ defmodule Ecto.Migrator do
     lock_for_migrations(lock, repo, opts, fun)
   end
 
-  defp pending_to(versions, migration_source, direction, target) do
+  defp pending_to(versions, migration_source, direction, target) when is_integer(target) do
     within_target_version? = fn
       {version, _, _}, target, :up ->
         version <= target
@@ -549,7 +549,7 @@ defmodule Ecto.Migrator do
     |> Enum.take_while(&(within_target_version?.(&1, target, direction)))
   end
 
-  defp pending_to_exclusive(versions, migration_source, direction, target) do
+  defp pending_to_exclusive(versions, migration_source, direction, target) when is_integer(target) do
     within_target_version? = fn
       {version, _, _}, target, :up ->
         version < target
@@ -561,7 +561,7 @@ defmodule Ecto.Migrator do
     |> Enum.take_while(&(within_target_version?.(&1, target, direction)))
   end
 
-  defp pending_step(versions, migration_source, direction, count) do
+  defp pending_step(versions, migration_source, direction, count) when is_integer(count) do
     pending_in_direction(versions, migration_source, direction)
     |> Enum.take(count)
   end
