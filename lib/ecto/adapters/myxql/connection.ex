393cad704cf80b9bccedb0f28b67e8754f2e2b69
Maarten van Vliet <maartenvanvliet@users.noreply.github.com>
2022-12-20 12:45:33 +0100

    Address feedback

--- lib/ecto/adapters/myxql/connection.ex
+++ lib/ecto/adapters/myxql/connection.ex
@@ -313,7 +313,7 @@ if Code.ensure_loaded?(MyXQL) do
 
     defp cte(%{with_ctes: _}, _), do: []
 
-    defp cte_expr({_name, _materialized, _cte}, _sources, _query) do when is_boolean(materialized)
+    defp cte_expr({_name, materialized, _cte}, _sources, query) when is_boolean(materialized) do
       error!(query, "MySQL adapter does not support materialized CTEs")
     end
 
