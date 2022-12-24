0db6f82c1cd135dd1619640eb42a8e3bd8eb5441
Greg Rychlewski <grychle@LLL7WPM64V5M.ngco.com>
2022-12-24 08:40:23 -0500

    remove imports

--- integration_test/pg/migrations_test.exs
+++ integration_test/pg/migrations_test.exs
@@ -4,8 +4,6 @@ defmodule Ecto.Integration.MigrationsTest do
   alias Ecto.Integration.PoolRepo
   alias Ecto.Integration.AdvisoryLockPoolRepo
   import ExUnit.CaptureLog
-  import Support.FileHelpers
-  import Ecto.Migrator
 
   @moduletag :capture_log
   @base_migration 3_000_000
