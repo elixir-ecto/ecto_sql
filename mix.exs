879c7a610064f8d780f9d0acbcc311599834ea54
Greg Rychlewski <grychle@LLL7WPM64V5M.ngco.com>
2023-07-14 11:14:34 -0400

    update ecto

--- mix.exs
+++ mix.exs
@@ -77,7 +77,7 @@ defmodule EctoSQL.MixProject do
     if path = System.get_env("ECTO_PATH") do
       {:ecto, path: path}
     else
-      {:ecto, git: "https://github.com/greg-rychlewski/ecto.git", branch: "splice_fragment"}
+      {:ecto, git: "https://github.com/elixir-ecto/ecto.git"}
     end
   end
 
