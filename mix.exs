df6732a7f5fcd50aa2ddc6a09b2526038c763105
José Valim <jose.valim@dashbit.co>
2024-10-07 10:50:02 +0200

    Release v3.12.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule EctoSQL.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/elixir-ecto/ecto_sql"
-  @version "3.12.0"
+  @version "3.12.1"
   @adapters ~w(pg myxql tds)
 
   def project do
