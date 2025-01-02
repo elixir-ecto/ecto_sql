ec3472abf72f0914ea8aa41c02b10c0ed83148f2
Greg Rychlewski <greg.rychlewski@gmail.com>
2025-01-02 18:54:51 -0500

    oops

--- integration_test/pg/prepare_test.exs
+++ integration_test/pg/prepare_test.exs
@@ -8,18 +8,12 @@ defmodule Ecto.Integration.PrepareTest do
     one = TestRepo.insert!(%Post{title: "one"})
     two = TestRepo.insert!(%Post{title: "two"})
 
-    stmt_count_query = "SHOW GLOBAL STATUS LIKE '%prepared_stmt_count%'"
-
     # Uncached
     assert TestRepo.all(Post, prepare: :unnamed) == [one, two]
-    assert %{rows: [[0]]} = TestRepo.query(stmt_count_query, [])
     assert TestRepo.all(Post, prepare: :named) == [one, two]
-    assert %{rows: [[1]]} = TestRepo.query(stmt_count_query, [])
 
     # Cached
     assert TestRepo.all(Post, prepare: :unnamed) == [one, two]
-    assert %{rows: [[1]]} = TestRepo.query(stmt_count_query, [])
     assert TestRepo.all(Post, prepare: :named) == [one, two]
-    assert %{rows: [[1]]} = TestRepo.query(stmt_count_query, [])
   end
 end
