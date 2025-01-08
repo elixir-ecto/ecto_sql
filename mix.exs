8abb2e444fd7dc0f7efe33b48dfaee9fc02e031a
Greg Rychlewski <greg.rychlewski@gmail.com>
2025-01-08 14:17:46 -0500

    point to correct ecto

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule EctoSQL.MixProject do
     if path = System.get_env("ECTO_PATH") do
       {:ecto, path: path}
     else
-      {:ecto, git: "https://github.com/greg-rychlewski/ecto.git", branch: "literal_numbers"}
+      {:ecto, github: "elixir-ecto/ecto"}
     end
   end
 
