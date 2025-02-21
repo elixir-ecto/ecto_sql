1ed4f1cab9544cdfab06d455115a66a7d51cbe67
Greg Rychlewski <greg.rychlewski@gmail.com>
2025-02-20 20:37:56 -0500

    update docs

--- lib/ecto/adapters/postgres.ex
+++ lib/ecto/adapters/postgres.ex
@@ -120,8 +120,7 @@ defmodule Ecto.Adapters.Postgres do
   alongside Ecto, you must define a type module with your extensions.
   Create a new file anywhere in your application with the following:
 
-      Postgrex.Types.define(MyApp.PostgresTypes,
-                            [MyExtension.Foo, MyExtensionBar] ++ Ecto.Adapters.Postgres.extensions())
+      Postgrex.Types.define(MyApp.PostgresTypes, [MyExtension.Foo, MyExtensionBar])
 
   Once your type module is defined, you can configure the repository to use it:
 
