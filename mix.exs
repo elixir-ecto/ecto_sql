f08f46c97020ad3fc450f07e7c2cac0ab2b6e5d5
Greg Rychlewski <greg.rychlewski@gmail.com>
2025-09-28 23:40:34 -0400

    point to dev branch

--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule EctoSQL.MixProject do
     if path = System.get_env("ECTO_PATH") do
       {:ecto, path: path}
     else
-      {:ecto, "~> 3.13.0"}
+      {:ecto, git: "https://github.com/greg-rychlewski/ecto.git", branch: "fragment_tuple_source"}
     end
   end
 
