8acafac487710a6641b59adc8d9937300f942fbb
Greg Rychlewski <greg.rychlewski@gmail.com>
2025-09-30 07:37:26 -0400

    Allow fragment tuple sources in adapters (#691)

--- integration_test/sql/sql.exs
+++ integration_test/sql/sql.exs
@@ -7,7 +7,12 @@ defmodule Ecto.Integration.SQLTest do
   alias Ecto.Integration.Post
   alias Ecto.Integration.CorruptedPk
   alias Ecto.Integration.Tag
-  import Ecto.Query, only: [from: 2]
+  import Ecto.Query, only: [from: 2, from: 1]
+
+  test "fragment tuple sources" do
+    query = from f in {fragment("select 1 as num"), Barebone}
+    assert %Barebone{num: 1} = TestRepo.one(query)
+  end
 
   test "fragmented types" do
     datetime = ~N[2014-01-16 20:26:51]
--- mix.exs
+++ mix.exs
@@ -76,7 +76,7 @@ defmodule EctoSQL.MixProject do
     if path = System.get_env("ECTO_PATH") do
       {:ecto, path: path}
     else
-      {:ecto, "~> 3.13.0"}
+      {:ecto, git: "https://github.com/elixir-ecto/ecto.git", branch: "master"}
     end
   end
 
