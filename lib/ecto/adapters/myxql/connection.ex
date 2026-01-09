0aeb57f5785f98bf67b975cf5cb64180f6596d0b
Greg Rychlewski <greg.rychlewski@gmail.com>
2026-01-09 14:32:02 -0500

    get rid of fragment constant handling

--- lib/ecto/adapters/myxql/connection.ex
+++ lib/ecto/adapters/myxql/connection.ex
@@ -770,14 +770,6 @@ if Code.ensure_loaded?(MyXQL) do
       quote_name(literal)
     end
 
-    defp expr({:constant, _, [literal]}, _sources, _query) when is_binary(literal) do
-      [?', escape_string(literal), ?']
-    end
-
-    defp expr({:constant, _, [literal]}, _sources, _query) when is_number(literal) do
-      [to_string(literal)]
-    end
-
     defp expr({:splice, _, [{:^, _, [_, length]}]}, _sources, _query) do
       Enum.intersperse(List.duplicate(??, length), ?,)
     end
